5e8e19a589c333fccb789e5bff4116c71d6ca740
ahmed darder <91543865+Drdr1@users.noreply.github.com>
2025-04-03 17:57:58 +0200

    Update outputs.tf

--- azure-openai-platform/terraform/outputs.tf
+++ azure-openai-platform/terraform/outputs.tf
@@ -5,3 +5,11 @@ output "openai_endpoint" {
 output "databricks_workspace_url" {
   value = azurerm_databricks_workspace.databricks.workspace_url
 }
+
+output "kong_fqdn" {
+  value = azurerm_container_group.kong.fqdn
+}
+
+output "appgw_public_ip" {
+  value = azurerm_public_ip.appgw_pip.ip_address
+}
